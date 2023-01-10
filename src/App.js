9f8adff2d7a43dd9498f59c9fc58cf1dfe43ef16
ezeigboemmanuel <ezeigboemmanuel2@gmail.com>
2023-01-10 15:27:25 -0600

    added the import statement for all the components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from "react";
 import './App.css';
+import Form from "./components/Form";
+import Header from "./components/Header";
+import Image from "./components/Image";
 
 
 class App extends React.Component {
